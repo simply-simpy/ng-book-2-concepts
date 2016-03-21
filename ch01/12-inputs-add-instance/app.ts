07995121f1ee91ce90d3e660452a49dce43599b2
scott simpson <scott@Snapper.local>
2016-03-21 15:54:46 -0700

    add instance

---
+++ ch01/12-inputs-add-instance/app.ts
@@ -0,0 +1,52 @@
+import {Component} from 'angular2/core';
+import {InputComponent} from './input.component';
+
+class Dog {
+    age: string;
+    name: string;
+    constructor(age: string, name:string ){
+        this.age = age;
+        this.name = name;
+    }
+}
+
+@Component({
+    selector: 'my-app',
+    directives: [InputComponent],
+    template: `<the-input
+                         *ngFor = "#dog of dogs"
+                         [dog] = "dog"> 
+              </the-input>
+              
+              <form>
+                  <legend>Add Dog</legend>
+                      <label>Age
+                        <input type="text" #age>
+                      </label>
+                      <label>Name
+                        <input type="text" #name>
+                      </label>
+                      <button (click)="addDog(age, name)" type="submit">Add Dog</button>
+              </form>
+            `
+})
+
+export class MyApp{
+    dogs: Dog[];
+    constructor(){
+        this.dogs = [
+            new Dog("10", "Maggie"),
+            new Dog("8", "Buster")
+        ]
+    }
+
+    addDog(age: HTMLInputElement, name: HTMLInputElement): void {
+        console.log(`Adding dog age: ${age.value} and named: ${name.value}`);
+        this.dogs.push(new Dog(age.value, name.value));
+        age.value = '';
+        name.value = '';
+    }
+}
+
+
+
