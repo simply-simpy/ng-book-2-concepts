64992407a458c70a4df1b6c7a4ab08597c730b10
scott simpson <scott@Snapper.local>
2016-06-20 10:44:07 -0700

    add selectable item on list of members

---
+++ ch03/inputs-short-name-select/app.ts
@@ -0,0 +1,39 @@
+import {Component} from '@angular/core';
+
+class Member {
+    constructor(
+        public name: string,
+        public phone: string
+    ){}
+}
+
+@Component({
+    selector: 'the-member',
+    template: `<p>The member's name is {{ member.name}} and their phone number is {{ member.phone }}</p>`,
+    inputs: ['member: m']
+})
+class TheMember {
+    member: Member
+}
+
+@Component({
+    selector: 'inventory-app',
+    directives: [TheMember],
+    template: `
+        <div *ngFor="let member of members">
+            <the-member (click)="productWasSelected($event)" [m]="member"></the-member>
+        </div>
+        `
+})
+export class InventoryApp {
+    members: Member[];
+    constructor(){
+        this.members = [
+            new Member('Scott', '555-555-5555'),
+            new Member('Buster', '555-123-4567'),
+        ]
+    }
+    productWasSelected(member: Member): void {
+        console.log('Member clicked: ', member)
+    }
+}
