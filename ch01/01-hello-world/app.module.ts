e88c8c934e63288fd9adffe75bb336e29887d41d
scott simpson <scott@scott.local>
2016-09-15 08:23:51 -0700

    ready to update to angular 2

--- ch01/01-hello-world/app.module.ts
+++ ch01/01-hello-world/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HelloWorldComponent }  from './helloWorld.component';
+
 @NgModule({
     imports:      [ BrowserModule ],
     declarations: [ HelloWorldComponent ],
     bootstrap:    [ HelloWorldComponent ]
 })
+
 export class AppModule { }
