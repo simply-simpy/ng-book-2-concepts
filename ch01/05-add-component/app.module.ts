2695a9fe707f9c0fca813891af82950547b56360
scott simpson <scott@scott.local>
2016-09-25 08:06:04 -0700

    add ParagraphComponent to declarations in 05AddComponent

--- ch01/05-add-component/app.module.ts
+++ ch01/05-add-component/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BindActionComponent }  from './bindAction.component';
+import {ParagraphComponent} from "./paragraph.component";
 @NgModule({
     imports:      [ BrowserModule ],
-    declarations: [ BindActionComponent ],
+    declarations: [ BindActionComponent, ParagraphComponent ],
     bootstrap:    [ BindActionComponent ]
 })
 export class AppModule { }
